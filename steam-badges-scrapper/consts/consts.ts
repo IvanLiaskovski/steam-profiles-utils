7b06c5a6a2bd9dee9195e29077f7a097e88b99fa
Ivan Liaskovski <iliaskovski@gmail.com>
2024-03-27 23:12:02 +0100

    Steam profile Utils, badge scrapper, consts, urls

---
+++ steam-badges-scrapper/consts/consts.ts
@@ -0,0 +1,3 @@
+export const gameWithBadgesUrl =
+  "https://raw.githubusercontent.com/nolddor/steam-badges-db/main/data/badges.min.json";
+export const scrappedSiteUrl = `https://www.steamcardexchange.net/index.php?gamepage-appid-`;
