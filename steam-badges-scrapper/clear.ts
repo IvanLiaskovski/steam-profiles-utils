b16c264c50920d54fb47db0d57d5d7e698b15878
Ivan Liaskovski <iliaskovski@gmail.com>
2024-03-27 23:12:55 +0100

    Steam ProfileUtils, badge scrapper, clear json files functionality

---
+++ steam-badges-scrapper/clear.ts
@@ -0,0 +1,13 @@
+import clearFile from "./utils/clearFile";
+
+clearFile("./data/animatedAvatars/animatedAvatars.json");
+clearFile("./data/animatedBackgrounds/animatedBackgrounds.json");
+clearFile("./data/animatedFrames/animatedFrames.json");
+clearFile("./data/animatedMiniBackgrounds/animatedMiniBackgrounds.json");
+clearFile("./data/backgrounds/backgrounds.json");
+clearFile("./data/badges/badges.json");
+clearFile("./data/busterPacks/busterPacks.json");
+clearFile("./data/cards/cards.json");
+clearFile("./data/emoticons/emoticons.json");
+clearFile("./data/stickers/stickers.json");
+clearFile("./data/ScrappedApps/ScrappedApps.json");
